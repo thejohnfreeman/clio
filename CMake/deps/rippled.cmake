cc93435efcb4874ccefbba516a3f077a38443bf5
Alex Kremer <akremer@ripple.com>
2023-07-14 17:35:47 +0100

    Use CONAN_PKG for linking deps

--- CMake/deps/rippled.cmake
+++ CMake/deps/rippled.cmake
@@ -1,30 +1,11 @@
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_CLIO-XRPL})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_CLIO-XRPL})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_CLIO-XRPL})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_ABSEIL})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_ABSEIL})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_ABSEIL})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_GRPC} ${CONAN_LIB_DIRS_PROTOBUF})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_GRPC} ${CONAN_LIBS_PROTOBUF})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_GRPC} ${CONAN_INCLUDE_DIRS_PROTOBUF})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_LIBUV})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_LIBUV})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_LIBUV})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_C-ARES})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_C-ARES})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_C-ARES})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_ZLIB})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_ZLIB})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_ZLIB})
-
-target_link_directories(clio PUBLIC ${CONAN_LIB_DIRS_RE2})
-target_link_libraries(clio PUBLIC ${CONAN_LIBS_RE2})
-target_include_directories(clio PUBLIC ${CONAN_INCLUDE_DIRS_RE2})
+target_link_libraries(clio PUBLIC CONAN_PKG::clio-xrpl)
+target_link_libraries(clio PUBLIC CONAN_PKG::date)
+target_link_libraries(clio PUBLIC CONAN_PKG::abseil)
+target_link_libraries(clio PUBLIC CONAN_PKG::grpc) 
+target_link_libraries(clio PUBLIC CONAN_PKG::libuv) 
+target_link_libraries(clio PUBLIC CONAN_PKG::c-ares) 
+target_link_libraries(clio PUBLIC CONAN_PKG::zlib) 
+target_link_libraries(clio PUBLIC CONAN_PKG::re2) 
 
 find_package(OpenSSL 1.1.1 REQUIRED)
 set_target_properties(OpenSSL::SSL PROPERTIES
